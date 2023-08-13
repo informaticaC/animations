2cacd3703ed1021859e968739f62f8e6b6daa64b
Marcelo_Gabriel_Pesoa <pesoa01@gmail.com>
2023-08-13 09:42:19 -0300

    animation 2

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 const hiddenElement = document.querySelectorAll('.hidden');
-const link = document.querySelectorAll(".link");
+const links = document.querySelectorAll(".link");
+
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting) {
-            console.log(entry.target)
+            //console.log(entry.target.id)
             entry.target.classList.add('show');
+
+            links.forEach((link) =>{
+                const href = (link.getAttribute('href').split('').splice(1).join(''));
+                if (href === entry.target.id) {
+                    console.log(href);
+                    link.classList.add('link--active');
+                } else {
+                    link.classList.remove('link--active');
+                }
+
+            } )
             
         } else {
             entry.target.classList.remove('show');
         };
     })
-},{threshold: 0.6})
+},{threshold: 0.5})
 
 // console.log(hiddenElement);
 
